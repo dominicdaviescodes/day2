d918a0b3c4ec4358f183059bcce08bcd6a86fd51
dominicdaviescodes <codewithdom@gmail.com>
2020-12-02 21:05:34 +0000

    don't let next be clicked over 4 times

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 // more than one
 const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+// don't let next be clicked over 4 times
+next.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  console.log(currentActive);
+});
