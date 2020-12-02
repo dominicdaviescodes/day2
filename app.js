d8476861fa7338e9302c1255f25514f5d3a98383
dominicdaviescodes <codewithdom@gmail.com>
2020-12-02 18:28:38 +0000

    grab DOM elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// DOM elements
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+// more than one
+const circles = document.querySelectorAll('.circle');
